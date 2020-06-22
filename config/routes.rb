62a3cec37888ec30c9c74a5bb5782a276c3b416c
Flora <flora.e.hewitt@gmail.com>
2020-06-22 18:20:05 +0400

    added login and sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
 
   get '/property/:id' => 'static_pages#property'
+  get '/login' => 'static_pages#login'
 
   namespace :api do
     # Add routes below this line
