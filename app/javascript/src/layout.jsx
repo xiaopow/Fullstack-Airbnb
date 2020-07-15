c77192da6465237bc0f72490168aab95b944c130
Flora <flora.e.hewitt@gmail.com>
2020-07-15 16:33:46 +0400

    took out the 'Home' from Navbar as the logo already redirects to home

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -6,9 +6,6 @@ const Layout = (props) => {
         <a href="/"><span className="navbar-brand mb-0 h1 text-danger">Airbnb</span></a>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="/">Home</a>
-            </li>
           </ul>
         </div>
       </nav>
